11c1bb237f9d630644505f6f72bafd6927125938
frank-lam <1002039945@qq.com>
2018-12-24 15:50:59 +0800

    修改部署文件

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f "https://${GH_TOKEN}@github.com/frank-lam/fullstack-site.git" master:gh-pages
+git push -f "https://${GH_TOKEN}@github.com/frank-lam/fullstack-tutorial.git" master:gh-pages
 
 cd -
