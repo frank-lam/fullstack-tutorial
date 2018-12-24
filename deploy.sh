38735a0526d4d12e3ddd3767e36dedb909db73c6
frank-lam <1002039945@qq.com>
2018-12-24 17:08:04 +0800

    no message

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,19 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+
+
 rm gh-pages -rf; mkdir gh-pages; cp -r notes/* gh-pages/;
 cd gh-pages;
 
+
+# 将子目录的所有图片全部复制到文档根目录中，解决docsify图片索引不到的问题
 rm assets/ -rf
 mkdir assets
 
 rm pics/ -rf
 mkdir pics
+
 
 for f in $(find ../notes/ -type f -print | grep assets)
   do
